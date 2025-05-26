63b4fe681f248198741856615ac6170a08e5d683
Your Name <you@example.com>
2025-05-26 15:27:44 +0200

    Add variable declarations and value printing for name, text, number, and bool

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,22 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name = "Asia"
+console.log(name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+const text = "Voto"
+const number = 10
+const bool = true
+console.log(text)
+console.log(number)
+console.log(bool)
+
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
